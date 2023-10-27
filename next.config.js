a7f73c62803d9a5c0a8f8189899a117809e5e212
Try Wijono <try.wijono@99.co>
2023-10-27 13:21:29 +0800

    update headers control in next config js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cdn-Cache-Control",
+            value: "public, max-age=30, stale-while-revalidate=300, stale-if-error=300",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=30, stale-while-revalidate=300, stale-if-error=300",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
